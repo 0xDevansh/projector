65ea41dcf5edadc6b96b7e2f47c7c2c75712cf21
0xDevansh <devanshkandpal54@gmail.com>
2024-12-28 13:17:43 +0530

    move to fastify

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,57 @@
-import * as process from 'node:process'
-import express from 'express'
+import type { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
+import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import type { IncomingMessage, Server, ServerResponse } from 'node:http'
+import { env, exit } from 'node:process'
+import helmet from '@fastify/helmet'
+import { config } from 'dotenv'
+import { fastify } from 'fastify'
 
-const app = express()
-const PORT = process.env.PORT || 3000
+// load env variables
+config()
 
-app.get('/', (req, res) => {
-  res.send({ data: 'Hello world!', error: null })
+const devLogger = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'HH:MM:ss Z',
+      ignore: 'pid,hostname',
+    },
+  },
+}
+
+// setup server, add middleware
+const server: FastifyInstance<Server, IncomingMessage, ServerResponse>
+  = fastify({ logger: env.ENV === 'dev' ? devLogger : true }).withTypeProvider<JsonSchemaToTsProvider>()
+
+server.register(helmet, { global: true })
+
+server.get('/', {
+  schema: {
+    response: {
+      default: {
+        type: 'object',
+        properties: {
+          error: {
+            type: 'string',
+            default: null,
+          },
+          data: {
+            type: 'string',
+            default: null,
+          },
+        },
+      },
+    },
+  },
+}, (request: FastifyRequest, reply: FastifyReply) => {
+  reply.code(200).send({ data: 'Hello World!', error: null })
 })
 
-app.listen(PORT, () => {
-  console.log(`Started express server on port ${PORT}`)
+// Run the server!
+server.listen({ port: Number.parseInt(env.PORT || '8080') }, (err, _address) => {
+  if (err) {
+    server.log.error(err)
+    exit(1)
+  }
+  // Server is now listening on ${address}
 })
