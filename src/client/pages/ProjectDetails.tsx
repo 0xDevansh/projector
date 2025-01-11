514b544d01ac697b859e5859b38e4c1392a2774f
0xDevansh <devanshkandpal54@gmail.com>
2025-01-11 15:48:18 +0530

    Project details done

--- src/client/pages/ProjectDetails.tsx
+++ src/client/pages/ProjectDetails.tsx
@@ -1,10 +1,17 @@
+import type {
+  DegreeCode,
+  DeptCode,
+  ProjectDuration,
+  ProjectType,
+} from '../../types.js'
 import axios from 'axios'
-import { BanknoteIcon, BriefcaseIcon, CalendarIcon, ClockIcon, GraduationCapIcon, UserIcon } from 'lucide-react'
+import dayjs from 'dayjs'
+import { BanknoteIcon, BookCheckIcon, BriefcaseIcon, CalendarIcon, ClockIcon, GraduationCapIcon, SchoolIcon, UserIcon } from 'lucide-react'
 import React from 'react'
 import { useParams } from 'react-router'
 import useSWR from 'swr'
-import { type DeptCode, type ProjectType, projectType } from '../../types.js'
-import { deptName } from '../../types.js'
+import { degreeName, deptName, projectDuration, projectType,
+} from '../../types.js'
 import { Badge, BadgeWithTooltip } from '../components/ui/badge.js'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card.js'
 import { Separator } from '../components/ui/separator.js'
@@ -50,15 +57,30 @@ export default function ProjectDetails() {
                     {project.type.map((type: ProjectType) => {
                       const pType = ['disa', 'sura'].includes(type)
                         ? type.toUpperCase()
-                        : type[0].toUpperCase() + project.type.substring(1)
+                        : type[0].toUpperCase() + type.substring(1)
 
-                      return <BadgeWithTooltip key={type} tooltipText={projectType[type]} variant="outline">{pType}</BadgeWithTooltip>
+                      return (
+                        <BadgeWithTooltip
+                          key={type}
+                          tooltipText={projectType[type]}
+                          variant="outline"
+                        >
+                          {pType}
+                        </BadgeWithTooltip>
+                      )
                     })}
                   </div>
                   <div className="flex items-center gap-2">
                     <ClockIcon className="w-5 h-5 text-muted-foreground" />
                     <span className="font-semibold">Duration:</span>
-                    <span>{project.duration}</span>
+                    {project.duration.map((dur: ProjectDuration) => (
+                      <Badge
+                        key={dur}
+                        variant="outline"
+                      >
+                        {projectDuration[dur]}
+                      </Badge>
+                    ))}
                   </div>
                   <div className="flex items-center gap-2">
                     <UserIcon className="w-5 h-5 text-muted-foreground" />
@@ -68,20 +90,85 @@ export default function ProjectDetails() {
                   <div className="flex items-center gap-2">
                     <CalendarIcon className="w-5 h-5 text-muted-foreground" />
                     <span className="font-semibold">Last Apply Date:</span>
-                    <span>{project.lastApplyDate}</span>
+                    <span>{dayjs(project.lastApplyDate).format('DD MMM YYYY')}</span>
                   </div>
                   <div className="flex items-center gap-2">
                     <BanknoteIcon className="w-5 h-5 text-muted-foreground" />
                     <span className="font-semibold">Stipend:</span>
-                    <Badge variant={project.stipendProvided ? 'default' : 'outline'}>
-                      {project.stipendProvided ? 'Provided' : 'Not Provided'}
-                    </Badge>
+                    <span>{project.stipendProvided ? `₹${project.stipendAmount}` : 'Not Provided'}</span>
                   </div>
                 </div>
                 <Separator className="my-6" />
                 <div>
                   <h3 className="text-xl font-semibold mb-2">Description:</h3>
                   <p className="text-muted-foreground whitespace-pre-wrap">{project.description}</p>
+                </div>
+                <Separator className="my-6" />
+                <div className="grid gap-4">
+                  <h3 className="text-xl font-semibold mb-2">Eligibility Criteria</h3>
+                  {project.minCgpa && (
+                    <div className="flex items-center gap-2">
+                      <BookCheckIcon className="w-5 h-5 text-muted-foreground" />
+                      <span className="font-semibold">Minimum CGPA:</span>
+                      <span>{project.minCgpa}</span>
+                    </div>
+                  )}
+                  {project.minYear && (
+                    <div className="flex items-center gap-2">
+                      <BookCheckIcon className="w-5 h-5 text-muted-foreground" />
+                      <span className="font-semibold">Minimum year of study:</span>
+                      <span>{project.minYear}</span>
+                    </div>
+                  )}
+                  <div className="flex items-center gap-2">
+                    <GraduationCapIcon className="w-5 h-5 text-muted-foreground" />
+                    <span className="font-semibold">Eligible degrees:</span>
+                    {project.eligibleDegrees?.length > 0
+                      ? project.eligibleDegrees.map((ed: DegreeCode) => (
+                          <Badge
+                            key={ed}
+                            variant="outline"
+                          >
+                            {degreeName[ed]}
+                          </Badge>
+                        ))
+                      : 'All are eligible'}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <SchoolIcon className="w-5 h-5 text-muted-foreground" />
+                    <span className="font-semibold">Eligible departments:</span>
+                    {project.eligibleDegrees?.length > 0
+                      ? project.eligibleDepartments.map((ed: DeptCode) => (
+                          <Badge
+                            key={ed}
+                            variant="outline"
+                          >
+                            {deptName[ed]}
+                          </Badge>
+                        ))
+                      : 'All are eligible'}
+                  </div>
+                  {project.prerequisites && (
+                    <div>
+                      <Separator className="my-6" />
+                      <h3 className="text-xl font-semibold mb-2">Prerequisites</h3>
+                      <p className="text-muted-foreground whitespace-pre-wrap">{project.prerequisites}</p>
+                    </div>
+                  )}
+                  {project.selectionProcedure && (
+                    <div>
+                      <Separator className="my-6" />
+                      <h3 className="text-xl font-semibold mb-2">Selection Procedure</h3>
+                      <p className="text-muted-foreground whitespace-pre-wrap">{project.selectionProcedure}</p>
+                    </div>
+                  )}
+                  {project.learningOutcomes && (
+                    <div>
+                      <Separator className="my-6" />
+                      <h3 className="text-xl font-semibold mb-2">Learning Outcomes</h3>
+                      <p className="text-muted-foreground whitespace-pre-wrap">{project.learningOutcomes}</p>
+                    </div>
+                  )}
                 </div>
               </CardContent>
             </Card>
