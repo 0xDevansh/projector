8c303d1268a4451968c73cb4c014976f4a99ef87
0xDevansh <devanshkandpal54@gmail.com>
2025-01-23 22:16:12 +0530

    add resume to application

--- src/client/pages/ApplicationDetails.tsx
+++ src/client/pages/ApplicationDetails.tsx
@@ -1,13 +1,15 @@
 import type { Application } from '../../models/Application.js'
 
 import type { DegreeCode, DeptCode } from '../../types.js'
-import { Calendar, GraduationCapIcon, SchoolIcon, UserIcon } from 'lucide-react'
+import { Calendar, FileUserIcon, GraduationCapIcon, SchoolIcon, UserIcon } from 'lucide-react'
 import React from 'react'
 import { useParams } from 'react-router'
 import useSWR from 'swr'
 import { degreeName, deptName } from '../../types.js'
+import { buttonVariants } from '../components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card.js'
 import { Separator } from '../components/ui/separator.js'
+import { cn } from '../utils'
 import NotFound from './NotFound.js'
 
 export default function ApplicationDetails() {
@@ -53,15 +55,30 @@ export default function ApplicationDetails() {
               <span className="font-semibold">Year of joining:</span>
               <span>{`20${application.student.kerberos.substring(3, 5)}`}</span>
             </div>
+            <div className="flex items-center gap-2">
+              <FileUserIcon className="w-5 h-5 text-muted-foreground" />
+              <span className="font-semibold">Resume:</span>
+              {application.student.resumePath
+                ? (
+                    <a
+                      className={cn(buttonVariants({ variant: 'default' }), 'py-4')}
+                      href={`/api/user/${application.student.kerberos}/resume`}
+                    >
+                      View Resume
+                    </a>
+                  )
+                : <span>Not uploaded</span>}
+            </div>
+
             <div>
               <Separator className="my-6" />
               <h3 className="text-xl font-semibold mb-2">Relevant skills and experience</h3>
-              <p className="text-muted-foreground whitespace-pre-wrap">{application.relevantSkills}</p>
+              <p className="whitespace-pre-wrap">{application.relevantSkills}</p>
             </div>
             <div>
               <Separator className="my-6" />
               <h3 className="text-xl font-semibold mb-2">Satement of Purpose</h3>
-              <p className="text-muted-foreground whitespace-pre-wrap">{application.statementOfPurpose}</p>
+              <p className="whitespace-pre-wrap">{application.statementOfPurpose}</p>
             </div>
           </div>
         </CardContent>
