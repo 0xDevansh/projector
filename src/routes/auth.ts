48b9f1eb8b2f6b8e561b0ddbf51861a106bedff2
0xDevansh <devanshkandpal54@gmail.com>
2025-01-30 10:24:16 +0530

    fix response bug

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -69,8 +69,8 @@ export default async (server: FastifyInstance) => {
       if (response.status === 200) {
         console.log(response.data)
         const token = await jwtUser.sign({
-          email: response.data.user.email,
-          name: response.data.user.name,
+          email: response.data.email,
+          name: response.data.name,
         }, {
           expiresIn: 60 * 60 * 24,
         })
