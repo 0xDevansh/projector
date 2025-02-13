a90f7bf3de81a7e4461e760152ed7caa78a751d8
0xDevansh <devanshkandpal54@gmail.com>
2025-02-14 00:07:12 +0530

    add pm2 ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [{
+    name: 'projects',
+    script: 'dist/server.js',
+    args: '--dev',
+    watch: 'dist',
+    env: {
+      PORT: '8000',
+      http_proxy: 'repo.iitd.ac.in:9999',
+      HTTP_PROXY: 'repo.iitd.ac.in:9999',
+      https_proxy: 'repo.iitd.ac.in:9999',
+      HTTPS_PROXY: 'repo.iitd.ac.in:9999',
+    },
+  }, {
+    name: 'py-proxy',
+    script: 'req_proxy.py',
+    args: '5005',
+    watch: ['req_proxy.py'],
+    env: {
+      http_proxy: 'repo.iitd.ac.in:9999',
+      HTTP_PROXY: 'repo.iitd.ac.in:9999',
+      https_proxy: 'repo.iitd.ac.in:9999',
+      HTTPS_PROXY: 'repo.iitd.ac.in:9999',
+    },
+  }],
+}
