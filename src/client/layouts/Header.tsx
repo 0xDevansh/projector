fe80e112c3f6c6986d434a9b659f8447476e0798
0xDevansh <devanshkandpal54@gmail.com>
2025-02-15 12:36:25 +0530

    fix small oopsie

--- src/client/layouts/Header.tsx
+++ src/client/layouts/Header.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Link, NavLink } from 'react-router'
 import { AuthContext } from '../AuthContext.js'
 import { buttonVariants } from '../components/ui/button.js'
-import { cn } from '../utils'
+import { cn } from '../utils.js'
 
 export const prodLoginLink = 'https://oauthdevclub.vercel.app/signin?client_id=mHuhtM4zb6YlaY9qxEvNpvmEM4w&redirect_uri=https%3A%2F%2Fprojects.apps.iitd.ac.in%2Fapi%2Foauth-callback'
 export const devLoginLink = 'https://oauthdevclub.vercel.app/signin?client_id=mHuhtM4zb6YlaY9qxEvNpvmEM4w&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fapi%2Foauth-callback'
